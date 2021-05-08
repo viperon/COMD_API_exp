0d9badfbcdc2416ee170ae2a51f7411f665b1003
Viperon <viperon86@gmail.com>
2021-05-08 15:32:50 +0200

    first revision, split program into functions, and more reusable chunks. adding requirements.txt

--- COMDAPICON.py
+++ COMDAPICON.py
@@ -1,8 +1,4 @@
 # Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)]
-# pip install comdirect-api-simple==0.0.10
-# pip install pandas==1.2.3
-# pip install jupyterlab==3.0.9
-# current jupyter notebook file = COMD_dec_apr
 
 import time
 import os
@@ -11,30 +7,12 @@ import pandas as pd
 import numpy as np
 import smtplib
 from email.message import EmailMessage
-from comdirect_api.comdirect_client import ComdirectClient  # api wrapper, may not be well maintained
-
-csv_raw = 'TEST_jan-mar135_raw.csv'
+from comdirect_api.comdirect_client import ComdirectClient
 
 
-def write_csv(data):
-
-    try:
-        with open(csv_raw, 'a', encoding="utf-8") as document:
-            writer = csv.writer(document)
-            writer.writerow([
-                data['bookingDate'],
-                data['amount']['value'],
-                data['remitter'],
-                data['remittanceInfo'][:20],
-            ])
-    except TypeError as e:
-        print(e, '\n line missed')
-
-
-def main():
+def client_connection():
     """
-    Initialize comdirect API client, query transactions and write to csv
-    create pandas df to clean and organize data
+    Connect to Com-direct API, request transaction details
     """
     client_id = os.environ.get('COMD_client_id')
     client_secret = os.environ.get('COMD_client_secret')
@@ -45,151 +23,151 @@ def main():
     password = os.environ.get('COMD_password')
     client.fetch_tan(user, password)
 
-    time.sleep(60)  # allow time to get photoTAN activated
+    time.sleep(30)  # photoTAN activation
 
     client.activate_session()
     client.refresh_token()
 
     account_uuid = os.environ.get('COMD_account_uuid')
-
     transactions = client.get_account_transactions(
         account_uuid,
-        paging_count=135,  # 3 + months worth of transactions. Average monthly 45
+        paging_count=150,  # 135 == ~ 4 months worth of transactions
     )
 
-    for item in transactions['values']:
-        write_csv(item)
+    def parse_response():
+        """
+        Parse response into dictionary and dictionary into csv
+        """
+        transac_dict = dict()
+        for item in transactions['values']:
+            for k, v in item.items():
+                if k == 'bookingDate':
+                    transac_dict['Date'] = v
+                if k == 'amount':
+                    transac_dict['Amount'] = v['value']
+                if k == 'remitter':
+                    try:
+                        transac_dict['Description'] = v.get('holderName')
+                    except AttributeError:
+                        transac_dict['Description'] = 'None'
+                if k == 'remittanceInfo':
+                    transac_dict['Info'] = v[:18]
+
+            try:
+                with open('TESTCOMD_review1raw.csv', 'a', encoding='utf-8') as document:
+                    writer = csv.writer(document)
+
+                    writer.writerow(
+                        [transac_dict['Date'],
+                         transac_dict['Amount'],
+                         transac_dict['Description'],
+                         transac_dict['Info']
+                         ])
+            except TypeError as e:
+                print(e, '\n line missed')
+    parse_response()
 
 
-if __name__ == '__main__':
-    main()
+def data_prep():
 
-# DATA PREP - PANDAS
-df1 = pd.read_csv('TEST_jan-mar135_raw.csv', names=['Date', 'amount', 'short_desc', 'long_desc'])
-df2 = pd.read_excel('COMD_year_paypal.xlsx')  # paypal file currently jan-march 2021
-df1['short_desc'] = df1.short_desc.str.split(':', expand=True)[1]
-df1['short_desc'] = df1.short_desc.str.split('}', expand=True)[0]
+    na_vals = ['NA', 'None']
+    df1 = pd.read_csv('TESTCOMD_review1raw.csv', names=['Date', 'Amount', 'Description', 'Info'], skiprows=2,
+                      na_values=na_vals)
+    df2 = pd.read_excel('paypal_jan_apr21.xlsx')
+    df = df1.append(df2, ignore_index=True)
 
-# Merge COMD_API data with paypal transactions and sort by date
-df = df1.append(df2, ignore_index=True)
-df.sort_values(by='Date', inplace=True)
+    df.Description = np.where(df.Description.isnull(), df.Info, df.Description)
+    df['Description'].fillna(df['Info'])
+    df['Description'] = np.where(df['Description'].str.startswith('01'), df['Description'].str[2:], df['Description'])
+    df['Supermarkt'] = np.where(df['Description'].str.contains(
+        'EDEKA|PaySqu|PAYONE|NETTO|ALDI|Schäf|NAH UND|ZEIT FUER|ALNATUR|DER KUCH|CAFE LEBENS|BAECKER|Bio Kondit|unverpackt',
+        case=False), df['Amount'], np.nan)
+    df['Miete/Wohnen'] = np.where(df['Description'].str.contains(
+        'Telefonica|IKEA|Stadtwerke|OVAG|Miete|Helpling|Wohn|Glühbirne|Rundfunk|BAUHAUS|Betriebskos|Monatsabrech',
+        case=False), df['Amount'], np.nan)
+    df['Drogerie'] = np.where(df['Description'].str.contains(
+        'ROSSMANN|OEVERHAUS|Apo Doc|DROGERIE|APOTHEKE',
+        case=False), df['Amount'], np.nan)
+    df['Essen_gehen'] = np.where(df['Description'].str.contains(
+        'SUSHI|Funky Fisch|ISHIN|BURGER',
+        case=False), df['Amount'], np.nan)
+    df['Oliver'] = np.where(df['Info'].str.contains(
+        'Limango|Vinted|LANGERBLO|0121340000|M BERLIN|Baby',
+        case=False), df['Amount'], np.nan)
+    df['Reise/Freizeit'] = np.where(df['Description'].str.contains(
+        'Blocsport', case=False), df['Amount'], np.nan)
 
-# Add columns with respective amount as values - filter by keywords in transaction description
-df['Supermarkt'] = np.where(df['short_desc'].str.contains(
-    'EDEKA|PaySqu|PAYONE|NETTO|ALDI|Schäf|NAH UND|ZEIT FUER|ALNATUR|DER KUCH|CAFE LEBENS|BAECKER|Bio Kondit|unverpackt',
-    case=False), df['amount'], np.nan)
-df['Miete/Wohnen'] = np.where(df['short_desc'].str.contains(
-    'Telefonica|IKEA|Stadtwerke|OVAG|Miete|Helpling|Wohn|Glühbirne|Rundfunk|BAUHAUS', case=False), df['amount'], np.nan)
-df['Drogerie'] = np.where(df['short_desc'].str.contains(
-    'ROSSMANN|OEVERHAUS|Apo Doc|DROGERIE|APOTHEKE', case=False), df['amount'], np.nan)
-df['Essen_gehen'] = np.where(df['short_desc'].str.contains(
-    'SUSHI|Funky Fisch|ISHIN|BURGER', case=False), df['amount'], np.nan)
-df['Oliver'] = np.where(df['long_desc'].str.contains(
-    'Limango|Vinted|LANGERBLO|0121340000|M BERLIN', case=False), df['amount'], np.nan)
+    df.fillna(0, inplace=True)
+    df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+    df['Month'] = df['Date'].dt.month_name()
 
-# fill missing values with copies (short/long_desc)
-df.short_desc = np.where(df.short_desc.isnull(), df.long_desc, df.short_desc)
-# fill missing values with zeros(float) to be able to sum columns
-df.fillna(0, inplace=True)
+    def monthly_prep():
 
-# PREP MONTHLY DATA
-df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
-df['Month'] = df['Date'].dt.month_name()  # add 'Month' column for visual filtering
-df = df[[
-    'Date',
-    'Month',
-    'amount',
-    'short_desc',
-    'long_desc',
-    'Supermarkt',
-    'Miete/Wohnen',
-    'Drogerie',
-    'Essen_gehen',
-    'Oliver'
-]]
+        month_list = ['January', 'February', 'March', 'April']
+        df_months = dict()
+        for month in month_list:
+            df_months[month] = df[(df['Month'] == month)]
 
-# Monthly dataframes
-df_jan = df[(df['Month'] == 'January')]
-df_feb = df[(df['Month'] == 'February')]
-df_mar = df[(df['Month'] == 'March')]
-# df_apr = df[(df['Month'] == 'April')]
-# df_may = df[(df['Month'] == 'May')]
-# df_jun = df[(df['Month'] == 'June')]
-# df_jul = df[(df['Month'] == 'July')]
-# df_aug = df[(df['Month'] == 'August')]
-# df_sep = df[(df['Month'] == 'September')]
-# df_oct = df[(df['Month'] == 'October')]
-# df_nov = df[(df['Month'] == 'November')]
-# df_dec = df[(df['Month'] == 'December')]
+        # save to csv file - this specific for loop only works directly in jupyter nb
+        for key, value in df_months.items():
+            if key == month_list:
+                df_months[key].to_csv(f'test{key}data.csv')
 
-# save to csv file
-df_jan.to_csv('TESTjan21_data_full.csv')
-df_feb.to_csv('TESTfeb21_data_full.csv')
-df_mar.to_csv('TESTmar21_data_full.csv')
-# df_apr.to_csv('TESTapr21_data_full.csv')
-# df_may.to_csv('TESTmay21_data_full.csv')
-# df_jun.to_csv('TESTjun21_data_full.csv')
-# df_jul.to_csv('TESTjul21_data_full.csv')
-# df_aug.to_csv('TESTaug21_data_full.csv')
-# df_sep.to_csv('TESTsep21_data_full.csv')
-# df_oct.to_csv('TESToct21_data_full.csv')
-# df_nov.to_csv('TESTnov21_data_full.csv')
-# df_dec.to_csv('TESTdec21_data_full.csv')
+        # sum of DFs to csv
+        df_totals = dict()
+        for df_month in month_list:
+            df_totals[df_month] = {
+                'Supermarkt': df_months[df_month]['Supermarkt'].sum(),
+                'Oliver': df_months[df_month]['Oliver'].sum(),
+                'Drogerie': df_months[df_month]['Drogerie'].sum(),
+                'Miete/Wohnen': df_months[df_month]['Miete/Wohnen'].sum(),
+                'Essen_gehen': df_months[df_month]['Essen_gehen'].sum(),
+                'Total': df_months[df_month]['Amount'].sum(),
+            }
 
-# save sum of totals per month df to csv
-df_totals = {'March': {
-    'Supermarkt': df_mar['Supermarkt'].sum(),
-    'Oliver': df_mar['Oliver'].sum(),
-    'Drogerie': df_mar['Drogerie'].sum(),
-    'Miete/Wohnen': df_mar['Miete/Wohnen'].sum(),
-    'Essen_gehen': df_mar['Essen_gehen'].sum(),
-},
-            'February': {
-    'Supermarkt': df_feb['Supermarkt'].sum(),
-    'Oliver': df_feb['Oliver'].sum(),
-    'Drogerie': df_feb['Drogerie'].sum(),
-    'Miete/Wohnen': df_feb['Miete/Wohnen'].sum(),
-    'Essen_gehen': df_feb['Essen_gehen'].sum(),
-},
-            'January': {
-    'Supermarkt': df_jan['Supermarkt'].sum(),
-    'Oliver': df_jan['Oliver'].sum(),
-    'Drogerie': df_jan['Drogerie'].sum(),
-    'Miete/Wohnen': df_jan['Miete/Wohnen'].sum(),
-    'Essen_gehen': df_jan['Essen_gehen'].sum(),
-}}
+            df_sum = pd.DataFrame.from_dict(df_totals)
+            df_sum.to_csv('TESTyear_totals_21_data1.csv')
 
-df_sum = pd.DataFrame.from_dict(df_totals)
-df_sum.to_csv('TESTyear_totals_21_data1.csv')
+    monthly_prep()
 
-# EMAIL FILES
 
-EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
-EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+def email_files():
 
-contacts = [EMAIL_ADDRESS]
+    EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
+    EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
-msg = EmailMessage()
-msg['Subject'] = 'Comdirect files'
-msg['From'] = EMAIL_ADDRESS
-msg['To'] = 'testmail@protonmail.com'
-msg.set_content('files attached.')
+    msg = EmailMessage()
+    msg['Subject'] = 'Comdirect files'
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = 'testmailv1@protonmail.com'
+    msg.set_content('files attached.')
 
-files = [
-    'TESTyear_totals_21_data1.csv',
-    'TESTjan21_data_full.csv',
-    'TESTfeb21_data_full.csv',
-    'TESTmar21_data_full.csv'
-]
+    files = [
+        'TESTyear_totals_21_data1.csv',
+        # 'testJanuarydata.csv',
+        # 'testFebruarydata.csv',
+        # 'testMarchdata.csv',
+        # 'testAprildata.csv',
+    ]
 
-for file in files:
-    with open(file, 'rb') as f:
-        file_data = f.read()
-        file_type = f.name
-        file_name = f.name
+    for file in files:
+        with open(file, 'rb') as f:
+            file_data = f.read()
+            file_type = f.name
+            file_name = f.name
 
-    msg.add_attachment(file_data, maintype='text', subtype=file_type, filename=file_name)
+        msg.add_attachment(file_data, maintype='text', subtype=file_type, filename=file_name)
 
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-    smtp.send_message(msg)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.send_message(msg)
+
+
+def main():
+
+    client_connection()
+    data_prep()
+    email_files()
+
+
+main()
